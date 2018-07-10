341c3f0211840f6ffd1a47321690e879308f3845
agostini335 <andrea1.agostini@mail.polimi.it>
2018-07-10 12:39:26 +0200

    Start

---
+++ src/Launcher.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Launcher {
+    public static void main(String[] args) {
+        String n;
+        String c;
+        Persona andrea ;
+        andrea=new Persona(1);
+        Persona dario = new Persona(2) ;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 2; i++) {
+            System.out.println("inserisci nome persona " + (i + 1));
+            n = sc.next();
+            System.out.println("inserisci cognome persona " + (i + 1));
+            c = sc.next();
+            if (i == 0) {
+                andrea.setNome(n);
+                andrea.setCognome(c);
+            } else {
+                dario.setNome(n);
+                dario.setCognome(c);
+            }
+        }
+
+        System.out.println("il nome della prima persona è " + andrea.getNome());
+        System.out.println("il cognome della prima persona è " + andrea.getCognome());
+        System.out.println("il nome della seconda persona è " + dario.getNome());
+        System.out.println("il cognome della seconda persona è " + dario.getCognome());
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,33 @@
+public class Persona {
+    private long codice ;
+    private String nome ;
+    private String cognome;
+
+    public Persona(int codice){
+        nome="la persona non ha un nome";
+        this.codice=(long) codice;
+    }
+
+    public void setCodice (long l) {
+        codice = l ;
+    }
+    public long getCodice () {
+        return codice ;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+}
